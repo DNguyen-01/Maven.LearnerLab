592750cf0599a26ff8a3d9006aeacd770da35638
David Nguyen <davebox309@gmail.com>
2021-11-20 09:51:19 -0500

    peopleTest completed

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,10 +1,18 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class People {
+public class People implements Iterable<Person> {
 
-    private List<Person> personList;
+    private final List<Person> personList;
+
+    public People(){
+
+        this.personList = new ArrayList<>();
+
+    }
 
     public void add(Person person) {
         personList.add(person);
@@ -33,10 +41,36 @@ public class People {
 
     }
 
-    public void remove(Long id){
+    public void remove(Long id) {
 
         personList.removeIf(person -> person.getId().equals(id));
+        //originally a for(loop)
+//        for (Person person : personList){
+//            if(person.getId().equals(id))
+//                remove(id);}
     }
+
+    public void removeAll(){
+
+        personList.clear();
+    }
+
+    public int count(){
+
+        return personList.size();
+    }
+
+    public Object[] toArray(){
+        //return an array of personList because personList is an Arraylist
+        return personList.toArray();
+    }
+
+    public Iterator<Person> iterator(){
+
+        return personList.iterator();
+
+    }
+
 
 
 }
--- src/test/java/io/zipcoder/interfaces/PeopleTest.java
+++ src/test/java/io/zipcoder/interfaces/PeopleTest.java
@@ -1,18 +1,57 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 public class PeopleTest {
 
-    private List<Person> personList ;
+    private People people  ;
+    private Person person1;
+    private Person person2;
+
+
+    @Before
+    public void setUp(){
+
+        people = new People();
+        person1 = new Person("dave", 111L);
+        person2 = new Person("anna", 112L);
+
+    }
+
 
     @Test
     public void testAdd(){
-        //given - add a person to the list
+        //given
         //when
+        people.add(person1);
         //then
+        Assert.assertTrue(people.contains(person1));
     }
+
+    @Test
+    public void testRemove(){
+        //given
+        //when
+        people.add(person2); //add the person to the array
+        people.remove(person2); //remove the person
+        //then
+        Assert.assertFalse(people.contains(person2)); //check if the person is still there
+    }
+
+
+    @Test
+    public void testFindByID(){
+        //given
+        //when
+        Person actual = people.findByID(person1.getId());
+        //then
+        Assert.assertEquals(person1,actual);
+        Assert.assertNull(people.findByID(null));
+    }
+
 
 }
